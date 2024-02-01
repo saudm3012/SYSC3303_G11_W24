1a2251b6e4cd7a5b50be39519433dac2bf47a5e0
Mohammad Saud <mohammadsaud@cmail.carleton.ca>
2024-01-31 23:33:44 -0500

    ElevatorData is only one class, scheduler now receiving from floor

--- ElevatorData.java
+++ ElevatorData.java
@@ -1,4 +1,4 @@
-import java.io.Serializable;
+import java.io.*;
 import java.time.LocalTime;
 
 public class ElevatorData implements Serializable {
@@ -13,6 +13,51 @@ public class ElevatorData implements Serializable {
         this.up_button = true;
         this.car_button = 4;
     }
+    public byte[] elevatordata_to_bytes() throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream out = null;
+        try {
+            out = new ObjectOutputStream(bos);
+            out.writeObject(this);
+            out.flush();
+            return bos.toByteArray();
+        } catch(IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        } finally {
+            try {
+                bos.close();
+            } catch (IOException ex) {
+                // ignore close exception
+            }
+        }
+        return new byte[0]; /* Should not run */
+    }
+
+    public void bytes_to_elevatordata(byte[] data_packet) throws IOException {
+        ByteArrayInputStream bis = new ByteArrayInputStream(data_packet);
+        ObjectInput in = null;
+        try {
+            in = new ObjectInputStream(bis);
+            ElevatorData temp = (ElevatorData) in.readObject();
+            this.time = temp.time;
+            this.floor = temp.floor;
+            this.up_button = temp.up_button;
+            this.car_button = temp.car_button;
+            in.close();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+                // ignore close exception
+            }
+        }
+    }
+
 
 }
 
