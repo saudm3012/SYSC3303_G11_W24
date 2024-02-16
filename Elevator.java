df57e759f8498093dafbe139548b322fc5a17a9e
Zakariya Khan <zakariyakhan@cmail.carleton.ca>
2024-02-16 13:35:25 -0500

    Pseudo code for the elevator

--- Elevator.java
+++ Elevator.java
@@ -7,23 +7,60 @@
  public class Elevator implements Runnable
  {
      private ElevatorSocket socket; // communicator helper thread to send and receive data
-         
+     private ElevatorStates state;
      /**
       * The constructor for this class.
       */
      public Elevator () {
         socket = new ElevatorSocket(this);
+        this.state = ElevatorStates.IDLE;
      }
  
      public void processData(DataPacket receivedData) {
          // send data back to scheduler
          socket.send(receivedData);
      }
-  
+     
+     private void idle(){
+        // read and parse the next instruction from scheduler in the receive queue
+        // if theres no instruction stay idle 
+        state = ElevatorStates.IDLE;
+        // update the state: if the instruction is to move to a floor change to moving state
+        state = ElevatorStates.MOVING;
+        // else stop and unload/load
+        state = ElevatorStates.LOADING_UNLOADING;
+     }
+
+     private void move(){
+        // move to next floor in queue
+        // if we reach a DESTINATION floor change the state to load/unload
+        state = ElevatorStates.LOADING_UNLOADING;
+        // otherwise we reached a floor between our destination and need to ask scheduler if we should stop or continue moving
+        state = ElevatorStates.MOVING;
+     }
+     
+     private void loadAndUnload(){
+        //sleep
+        state = ElevatorStates.IDLE;
+     }
+
      public void run() {
          socket.start();
          while (true){
-            // do nothing but listen for packets using socket thread
+            switch (state) {
+                case IDLE:
+                    idle();
+                    break;
+                case MOVING:
+                    move();
+                    break;
+                case LOADING_UNLOADING:
+                    loadAndUnload();
+                    break;
+                default:
+                    idle();
+                    break;
+            }
          }
      }
  }
---
+++ ElevatorStates.java
@@ -0,0 +1,3 @@
+public enum ElevatorStates {
+    IDLE, MOVING, LOADING_UNLOADING
+}
